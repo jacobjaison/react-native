aab8d4fcc29e24584205ec8e34e1b222a6808fa1
Jaison Jacob <jacob.jaison@hotmail.com>
2023-03-27 19:02:57 +0400

    About Screen Changes

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,8 @@ import DirectoryScreen from './DirectoryScreen';
 import {createStackNavigator} from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
 
 const  Drawer = createDrawerNavigator();
 
@@ -13,6 +15,30 @@ const screenOptions = {
     headerStyle: {backgroundColor : '#5637DD'}
 }
 
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='About'
+                component={AboutScreen}
+                />
+        </Stack.Navigator>
+    )
+}
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+                name='Contact'
+                component={ContactScreen}
+                options={{title:'Contact Us'}}
+                />
+        </Stack.Navigator>
+    )
+}
 const HomeNavigator = () => {
     const Stack = createStackNavigator();
     return (
@@ -63,6 +89,16 @@ const Main = () => {
                         name='Directory'
                         component= {DirectoryNavigator}
                         options = {{title:'Directory'}}/>
+                        
+                    <Drawer.Screen
+                       name='About'
+                       component={AboutNavigator}/>
+
+                    <Drawer.Screen
+                      name='Contact'
+                      component={ContactNavigator}
+                      options = {{title:'Contact Us'}} />
+
                 </Drawer.Navigator>
         </View>
     
