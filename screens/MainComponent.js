8c60d1930b78c1e1be44920b9ece88f9d77d4786
Jaison Jacob <jacob.jaison@hotmail.com>
2023-04-01 07:15:00 +0400

    Reservation Changes

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -11,6 +11,7 @@ import {
 import HomeScreen from "./HomeScreen";
 import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
+import ReservationScreen from "./ReservationScreen";
 import { Icon } from "react-native-elements";
 import logo from "../assets/images/logo.png";
 import { useDispatch } from "react-redux";
@@ -71,6 +72,31 @@ const ContactNavigator = () => {
     </Stack.Navigator>
   );
 };
+
+const ReservationNavigator = () => {
+    const Stack = createStackNavigator();
+  
+    return (
+      <Stack.Navigator screenOptions={screenOptions}>
+        <Stack.Screen
+          name="Reservation"
+          component={ReservationScreen}
+          options={({ navigation }) => ({
+            title: "Reservation Search",
+            headerLeft: () => (
+              <Icon
+                name="tree"
+                type="font-awesome"
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    );
+  };
+
 const HomeNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -189,7 +215,22 @@ const Main = () => {
             ),
           }}
         />
-
+        <Drawer.Screen
+          name="ReserveCampsite"
+          component={ReservationNavigator}
+          options={{
+            title: "Reserve Campsite",
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="tree"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
         <Drawer.Screen
           name="About"
           component={AboutNavigator}
