5e1114677930a05c19020321046db5a1c1f27199
Jaison Jacob <jacob.jaison@hotmail.com>
2023-04-03 15:15:51 +0400

    Updated Chages

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -15,10 +15,37 @@ export const fetchComments = createAsyncThunk(
     }
 );
 
+export const postComment = createAsyncThunk(
+    'comments/postComments',
+    async(payload, {dispatch, getState}) => {
+        setTimeout(() => {
+            
+            const { comments } = getState();
+            const d = new Date();
+
+            payload.date = d.toISOString();
+            payload.id = comments.commentsArray.length;
+            dispatch(addComment(payload));
+        },2000);
+
+    }
+)
+
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state,action) => {
+
+            const newComment = {
+                id: state.commentsArray.length + 1,
+                ...action.payload
+            }
+
+            console.log('New Comment', newComment);
+            state.commentsArray.push(newComment);
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(fetchComments.pending, (state) => {
@@ -39,3 +66,4 @@ const commentsSlice = createSlice({
 });
 
 export const commentsReducer = commentsSlice.reducer;
+export const { addComment } = commentsSlice.actions;
