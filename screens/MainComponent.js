9c74eed8f9a4af24481f331a0224e2cecbd9d1a6
Jaison Jacob <jacob.jaison@hotmail.com>
2023-04-08 15:53:57 +0400

    updated changes

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -21,6 +21,8 @@ import { fetchCampsites } from "../features/campsites/campsitesSlice";
 import { fetchPromotions } from "../features/promotions/promotionsSlice";
 import { fetchComments } from "../features/comments/commentsSlice";
 import FavoritesScreen from "./FavoritesScreen"; 
+import LoginScreen  from "./LoginScreen";
+
 const Drawer = createDrawerNavigator();
 
 const screenOptions = {
@@ -49,6 +51,7 @@ const AboutNavigator = () => {
     </Stack.Navigator>
   );
 };
+
 const ContactNavigator = () => {
   const Stack = createStackNavigator();
 
@@ -121,6 +124,27 @@ const ReservationNavigator = () => {
     );
   };
 
+  const LoginNavigator = () => {
+    const Stack = createStackNavigator();  
+    return (
+      <Stack.Navigator screenOptions={screenOptions}>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <Icon
+                name="sign-in"
+                type="font-awesome"
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    );
+  };
 
 const HomeNavigator = () => {
   const Stack = createStackNavigator();
@@ -144,6 +168,7 @@ const HomeNavigator = () => {
     </Stack.Navigator>
   );
 };
+
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -207,6 +232,22 @@ const Main = () => {
         drawerContent={CustomDrawerContent}
         drawerStyle={{ backgroundColor: "#CEC8FF" }}
       >
+      <Drawer.Screen
+          name="Login"
+          component={LoginNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="sign-in"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            )
+          }}
+        />
+
         <Drawer.Screen
           name="Home"
           component={HomeNavigator}
